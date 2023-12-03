898375978dd8284516b873956733022aa05f2cbf
viveksharma1220 <viveksharma12200@gmail.com>
2023-12-03 18:21:26 +0530

    commit done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='w-full border shadow-md py-2 fixed top-0 bg-blue-300'>
       <header className='text-center'>
-      <h1 className='text-xl font-bold uppercase'>Aakanksha Blogs</h1>
+      <h1 className='text-xl font-bold uppercase'>Vivek Blogs</h1>
       </header>
     </div>
   )
